5eafb45f8411bc55792d154473f5daac4c126da4
Brian Whitton <me@brianwhitton.com>
2016-12-20 09:24:44 -0500

    partially wire up email requirement call
    save reject/resolve for later to be called by close action

--- addon/components/basic-info.js
+++ addon/components/basic-info.js
@@ -1,5 +1,6 @@
 import Component from 'ember-component';
 import layout from '../templates/components/basic-info';
+import RSVP from 'rsvp';
 import Changeset from 'ember-changeset';
 import lookupValidator from 'ember-changeset-validations';
 import validations from 'nypr-account-settings/validators/basic-info';
@@ -16,6 +17,16 @@ export default Component.extend({
     this.changeset = changeset;
   },
 
+  emailRequirement(component) {
+    component.set('isShowingModal', true);
+    return new RSVP.Promise((resolve, reject) => {
+      component.setProperties({
+        resolveModal: resolve,
+        rejectModal: reject
+      });
+    });
+  },
+  
   rollbackEmailField(changeset) {
     // work around to rollback specific fields
     let snapshot = changeset.snapshot();
@@ -34,18 +45,16 @@ export default Component.extend({
         changeset.set('confirmEmail', changeset.get('email'));
       }
       return changeset.validate().then(() => {
-        if (verifyEmail) {
-          return this.get('emailRequirement')()
+        if (changeset.get('isValid') && verifyEmail) {
+          return this.get('emailRequirement')(this)
             .then(() => {
-              this.set('isEditing', false);
-              return changeset.save();
+              return changeset.save().then(() => this.set('isEditing', false));
             })
             .catch(() => {
               this.rollbackEmailField(changeset);
             });
         } else if (changeset.get('isValid')) {
-          this.set('isEditing', false);
-          return changeset.save();
+          return changeset.save().then(() => this.set('isEditing', false));
         } else {
           return changeset.get('errors');
         }
@@ -54,6 +63,12 @@ export default Component.extend({
     rollback(changeset) {
       changeset.rollback();
       this.set('isEditing', false);
+    },
+    
+    closeModal() {
+      this.set('isShowingModal', false);
+      this.set('resolveModal', null);
+      this.get('rejectModal')();
     }
   }
 });
