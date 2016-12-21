60730593649979be4942f0c576dfddfab4bfca3e
Brian Whitton <me@brianwhitton.com>
2016-12-21 09:03:26 -0500

    initialize password-card with a changeset instead of passing it in

--- addon/components/nypr-accounts/password-card.js
+++ addon/components/nypr-accounts/password-card.js
@@ -1,9 +1,20 @@
 import Ember from 'ember';
 import layout from '../../templates/components/nypr-accounts/password-card';
+import validations from 'nypr-account-settings/validators/nypr-accounts/password';
+import lookupValidator from 'ember-changeset-validations';
+import Changeset from 'ember-changeset';
 
 export default Ember.Component.extend({
   layout,
   tagName: '',
+  
+  init() {
+    this._super(...arguments);
+    this.changeset = new Changeset({
+      currentPassword: '',
+      newPassword: ''
+    }, lookupValidator(validations), validations);
+  },
   
   actions: {
     save(changeset) {
