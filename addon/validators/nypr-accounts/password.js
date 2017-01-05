bb22360a8b5529cf8cf9fe5da0ea7b7d1307b3fe
Brian Whitton <me@brianwhitton.com>
2017-01-05 13:57:55 -0500

    new passwords must have at least one number

--- addon/validators/nypr-accounts/password.js
+++ addon/validators/nypr-accounts/password.js
@@ -1,8 +1,12 @@
 import {
-  validateLength,
+  validateFormat,
+  validateLength
 } from 'ember-changeset-validations/validators';
 
 export default {
   currentPassword:  validateLength({ min: 8 }),
-  newPassword:      validateLength({ min: 8 }),
+  newPassword:      [ 
+    validateLength({ min: 8 }),
+    validateFormat({ regex: /\d/ })
+  ]
 };
