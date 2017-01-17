d312a4da72bf997d02e93076587388a62577b85f
Brian Whitton <me@brianwhitton.com>
2017-01-17 09:06:18 -0500

    add boilerplate for user model in dummy app

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -2,11 +2,6 @@ import Route from 'ember-route';
 
 export default Route.extend({
   model() {
-    return this.store.createRecord('user', {
-      givenName: "Jane",
-      familyName: "Doe",
-      preferredUsername: "janedoe",
-      email: "jane@email.com"
-    });
+    return this.store.queryRecord('user', {me: true});
   }
 });
---
+++ tests/dummy/mirage/factories/user.js
@@ -0,0 +1,8 @@
+import { Factory } from 'ember-cli-mirage';
+
+export default Factory.extend({
+  given_name: 'Jane',
+  family_name: 'Doe',
+  preferred_username: 'janedoe',
+  email: 'jane@email.com'
+});
