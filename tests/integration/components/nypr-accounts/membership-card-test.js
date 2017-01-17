3979e2482184b73d95c935e55168d6f955187066
Michael Hearn <git@mikehearn.net>
2017-01-16 19:23:44 -0500

    Membership component test stubs

---
+++ tests/integration/components/nypr-accounts/membership-card-test.js
@@ -0,0 +1,57 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('nypr-accounts/membership-card', 'Integration | Component | nypr accounts/membership card', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{nypr-accounts/membership-card}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#nypr-accounts/membership-card}}
+      template block text
+    {{/nypr-accounts/membership-card}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
+
+test('displays membership details if membership active', function(assert) {
+
+});
+
+test('displays renewal message if recent member', function(assert) {
+
+});
+
+test('displays donation callout for non-members', function(assert) {
+
+});
+
+test('displays expiring warning on near-expired membership', function(assert) {
+
+});
+
+test('pledge update link is populated with order_id', function(assert) {
+
+});
+
+test('payment history link works', function(assert) {
+
+});
+
+test('displays full payment history', function(assert) {
+
+});
+
+test('displays help modal when clicked on', function(assert) {
+
+});
