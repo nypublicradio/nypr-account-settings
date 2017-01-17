61fae4a632bd36fdda1a987199f3d06ca79c7514
Brian Whitton <me@brianwhitton.com>
2017-01-17 09:05:43 -0500

    add preferredUsername to dummy app user model

--- tests/dummy/app/models/user.js
+++ tests/dummy/app/models/user.js
@@ -2,7 +2,8 @@ import DS from 'ember-data';
 const { Model, attr } = DS;
 
 export default Model.extend({
-  givenName:  attr('string'),
-  familyName: attr('string'),
-  email:      attr('string')
+  givenName:         attr('string'),
+  familyName:        attr('string'),
+  email:             attr('string'),
+  preferredUsername: attr('string')
 });
