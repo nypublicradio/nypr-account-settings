ae4af710972c92193a838aae91f41a6efcaae842
Brian Whitton <me@brianwhitton.com>
2017-01-18 18:26:49 -0500

    add new password validator: no blank spaces.

--- addon/validators/nypr-accounts/password.js
+++ addon/validators/nypr-accounts/password.js
@@ -7,6 +7,7 @@ export default {
   currentPassword:  validateLength({ min: 8 }),
   newPassword:      [ 
     validateLength({ min: 8, message: 'New password must be at least 8 characters.'}),
-    validateFormat({ regex: /\d/, message: 'New password must have at least 1 number.' })
+    validateFormat({ regex: /\d/, message: 'New password must have at least 1 number.' }),
+    validateFormat({ regex: /[\\S]+/, message: 'New password must not have any blank spaces.' })
   ]
 };
