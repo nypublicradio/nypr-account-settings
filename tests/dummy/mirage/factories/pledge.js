b71f3434fc94d8f568f8094a1ea6acb5dd4c8580
Michael Hearn <git@mikehearn.net>
2017-01-19 16:36:59 -0500

    Early commit of pledge mirage factories

--- tests/dummy/mirage/factories/pledge.js
+++ tests/dummy/mirage/factories/pledge.js
@@ -1,13 +1,10 @@
-import { Factory, faker } from 'ember-cli-mirage';
+import { Factory } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  fund: () => faker.random.arrayElement(["WNYC", "WQXR", "Radiolab"]),
-  orderPrice: () => faker.random.arrayElement([60, 72, 120, 12, 90, 100]),
-  orderDate: faker.date.past,
-  orderType: () => faker.random.arrayElement(["onetime", "sustainer"]),
-  orderKey: () => faker.random.uuid(),
+  orderPrice: 10.00,
+  orderDate: '2017-01-01',
+  orderType: 'onetime',
   premium: 'Brian Lehrer Animated Series',
   creditCardType: 'VISA',
   creditCardLast4Digits: '0302',
-  isActive: faker.random.boolean
 });
