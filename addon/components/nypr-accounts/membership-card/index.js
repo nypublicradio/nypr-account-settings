985d976565fadcddb800530a48fcdf0d83f6001a
Michael Hearn <git@mikehearn.net>
2017-01-25 16:07:57 -0500

    More detailed 'multiple sutaining donations' screen

--- addon/components/nypr-accounts/membership-card/index.js
+++ addon/components/nypr-accounts/membership-card/index.js
@@ -1,6 +1,25 @@
 import Ember from 'ember';
+import computed from 'ember-computed';
 import layout from '../../../templates/components/nypr-accounts/membership-card';
 
 export default Ember.Component.extend({
   layout,
+  activePledges: computed('pledges', function() {
+    return this.get('pledges').filterBy('isActive', true);
+  }),
+  numberOfActivePledges: computed('activePledges', function() {
+    return this.get('activePledges').length;
+  }),
+  numberOfTotalPledges: computed('pledges.length', function() {
+    return this.get('pledges.length');
+  }),
+  hasMultipleActivePledges: computed('numberOfActivePledges', function() {
+    return this.get('numberOfActivePledges') >= 2;
+  }),
+  hasSingleActivePledge: computed('numberOfActivePledges', function() {
+    return this.get('numberOfActivePledges') === 1;
+  }),
+  hasExpiredPledges: computed('numberOfTotalPledges', function() {
+    return this.get('numberOfTotalPledges');
+  }),
 });
