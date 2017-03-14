24bf77dada7171b71a0ee1c85280a548524368eb
walsh9 <walsh9@gmail.com>
2017-03-14 17:43:54 -0400

    Add email verification component

---
+++ addon/components/nypr-account-forms/verify.js
@@ -0,0 +1,46 @@
+import Component from 'ember-component';
+import computed from 'ember-computed';
+import get from 'ember-metal/get';
+import fetch from 'fetch';
+import { rejectUnsuccessfulResponses } from 'nypr-account-settings/utils/fetch-utils';
+import layout from '../../templates/components/nypr-account-forms/verify';
+import messages from 'nypr-account-settings/validations/nypr-accounts/custom-messages';
+
+export default Component.extend({
+  layout,
+  session: null,
+  verificationCode: null,
+  emailId: null,
+  authAPI: null,
+  onFailure: () => {},
+  onSuccess: () => {},
+  APIUrl: computed('authAPI', 'email_id', function() {
+    return `${get(this, 'authAPI')}/membership/email/${get(this, 'emailId')}/verify`;
+  }),
+  didReceiveAttrs() {
+    this.verifyEmail(get(this, 'APIUrl'), get(this, 'verificationCode'));
+  },
+  verifyEmail(url, verificationCode) {
+    this.get('session').authorize('authorizer:nypr', (header, value) => {
+      let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': value
+      };
+      let method = 'PATCH';
+      let mode = 'cors';
+      let body = JSON.stringify({verification_code: verificationCode});
+      fetch(url, {method, mode, headers, body})
+      .then(rejectUnsuccessfulResponses)
+      .then(() => {
+        get(this, 'onSuccess')();
+      })
+      .catch((e) => {
+        let errorMessage = messages.genericVerificationError;
+        if (e && get(e, 'errors.message')) {
+          errorMessage = e.errors.message;
+        }
+        get(this, 'onFailure')(errorMessage);
+      });
+    });
+  }
+});
---
+++ tests/integration/components/nypr-account-forms/verify-test.js
@@ -0,0 +1,143 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import { startMirage }  from 'dummy/initializers/ember-cli-mirage';
+import wait from 'ember-test-helpers/wait';
+import sinon from 'sinon';
+
+moduleForComponent('nypr-account-forms/verify', 'Integration | Component | nypr account forms/verify', {
+  integration: true,
+  beforeEach() {
+    this.server = startMirage();
+    this.session = { authorize(_, callback) { callback(); } };
+
+  },
+  afterEach() {
+    this.server.shutdown();
+  }
+});
+
+test('it sends the correct values to the endpoint to verify the account', function(assert) {
+  this.set('session', this.session);
+
+  const testEmailId = 'email';
+  const testVerification = 'QWERTYUIOP';
+  let authAPI = 'http://example.com';
+  this.set('emailId', testEmailId);
+  this.set('verificationCode', testVerification);
+  this.set('authAPI', authAPI);
+
+  let requestSpy = sinon.spy();
+  let url = `${authAPI}/membership/email/${testEmailId}/verify`;
+  this.server.patch(url, (schema, request) => {
+    requestSpy(request);
+    return {};
+  }, 200);
+
+  this.render(hbs`{{nypr-account-forms/verify
+    emailId=emailId
+    verificationCode=verificationCode
+    authAPI=authAPI
+    session=session}}`);
+
+  return wait().then(() => {
+    assert.equal(requestSpy.callCount, 1);
+    assert.deepEqual(JSON.parse(requestSpy.firstCall.args[0].requestBody), { verification_code: testVerification });
+  });
+});
+
+test('it calls the onSuccess action on success', function(assert) {
+  this.set('session', this.session);
+
+  const testEmailId = 'email';
+  const testVerification = 'QWERTYUIOP';
+  let authAPI = 'http://example.com';
+  let onSuccess = sinon.spy();
+  let onFailure = sinon.spy();
+
+  this.set('emailId', testEmailId);
+  this.set('verificationCode', testVerification);
+  this.set('authAPI', authAPI);
+  this.set('onSuccess', onSuccess);
+  this.set('onFailure', onFailure);
+
+  let url = `${authAPI}/membership/email/${testEmailId}/verify`;
+  this.server.patch(url, () => {return {};}, 200);
+
+  this.render(hbs`{{nypr-account-forms/verify
+    emailId=emailId
+    verificationCode=verificationCode
+    authAPI=authAPI
+    session=session
+    onSuccess=onSuccess
+    onFailure=onFailure}}`);
+
+  return wait().then(() => {
+    assert.equal(onSuccess.callCount, 1);
+    assert.equal(onFailure.callCount, 0);
+  });
+});
+
+test('it calls the onFailure action on failure', function(assert) {
+  this.set('session', this.session);
+
+  const testEmailId = 'email';
+  const testVerification = 'QWERTYUIOP';
+  let authAPI = 'http://example.com';
+  let onSuccess = sinon.spy();
+  let onFailure = sinon.spy();
+
+  this.set('emailId', testEmailId);
+  this.set('verificationCode', testVerification);
+  this.set('authAPI', authAPI);
+  this.set('onSuccess', onSuccess);
+  this.set('onFailure', onFailure);
+
+  let url = `${authAPI}/membership/email/${testEmailId}/verify`;
+  this.server.patch(url, () => {return {};}, 400);
+
+  this.render(hbs`{{nypr-account-forms/verify
+    emailId=emailId
+    verificationCode=verificationCode
+    authAPI=authAPI
+    session=session
+    onSuccess=onSuccess
+    onFailure=onFailure}}`);
+  return wait().then(() => {
+    assert.equal(onSuccess.callCount, 0);
+    assert.equal(onFailure.callCount, 1);
+  });
+});
+
+
+test('it calls the onFailure action with the server error message', function(assert) {
+  this.set('session', this.session);
+
+  const testEmailId = 'email';
+  const testVerification = 'QWERTYUIOP';
+  const serverErrorMessage = 'something went wrong';
+  let authAPI = 'http://example.com';
+  let onSuccess = sinon.spy();
+  let onFailure = sinon.spy();
+
+  this.set('emailId', testEmailId);
+  this.set('verificationCode', testVerification);
+  this.set('authAPI', authAPI);
+  this.set('onSuccess', onSuccess);
+  this.set('onFailure', onFailure);
+
+  let url = `${authAPI}/membership/email/${testEmailId}/verify`;
+  this.server.patch(url, () => {return { errors: {code: 'ServerError', message: serverErrorMessage } };}, 400);
+
+  this.render(hbs`{{nypr-account-forms/verify
+    emailId=emailId
+    verificationCode=verificationCode
+    authAPI=authAPI
+    session=session
+    onSuccess=onSuccess
+    onFailure=onFailure}}`);
+  return wait().then(() => {
+    assert.equal(onSuccess.callCount, 0);
+    assert.equal(onFailure.callCount, 1);
+    assert.equal(onFailure.firstCall.args[0], serverErrorMessage);
+  });
+});
