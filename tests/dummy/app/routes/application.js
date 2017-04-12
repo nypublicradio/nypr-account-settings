13246797ced50cd2f21eaa0e7691ca4c73924de2
Michael Hearn <git@mikehearn.net>
2017-04-12 16:34:39 -0400

    Updating dummy app to use setupController for pledges

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,12 +1,14 @@
 import Route from 'ember-route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
   model() {
-    return this.store.queryRecord('user', { me: true }).then(result => {
-      return {
-        user: result,
-        pledge: this.store.findAll('pledge')
-      };
+    return RSVP.hash({
+      user: this.store.queryRecord('user', { me: true })
     });
+  },
+  setupController(controller, model) {
+    controller.set('pledges', this.store.findAll('pledge'));
+    return this._super(controller, model);
   }
 });
