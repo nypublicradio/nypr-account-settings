669ad9b90dd39092ac628030df5e3d1ceb05c3cd
Michael Hearn <git@mikehearn.net>
2017-04-26 15:30:20 -0400

    Update membership-card component computed vals

--- addon/components/nypr-accounts/membership-card.js
+++ addon/components/nypr-accounts/membership-card.js
@@ -1,41 +1,58 @@
-import Ember from 'ember';
-import computed from 'ember-computed';
-import { getCookie }  from 'nypr-account-settings/utils/cookies';
-import layout
-  from '../../templates/components/nypr-accounts/membership-card';
+import Ember from "ember";
+import computed from "ember-computed";
+import { getCookie } from "nypr-account-settings/utils/cookies";
+import layout from "../../templates/components/nypr-accounts/membership-card";
 
 export default Ember.Component.extend({
   layout,
-  orderDateSorting: ['orderDate:desc'],
-  sortedPledges: computed.sort('pledges', 'orderDateSorting'),
-  sortedSustainingPledges: computed.filterBy('sortedPledges', 'orderType', 'sustainer'),
-  sortedOneTimePledges: computed.filterBy('sortedPledges', 'orderType', 'onetime'),
-  mostRecentPledge: computed.reads('sortedPledges.firstObject'),
-  activePledges: computed.filterBy('sortedPledges', 'isActiveMember', true),
+  orderDateSorting: ["orderDate:desc"],
+  sortedPledges: computed.sort("pledges", "orderDateSorting"),
+  sortedSustainingPledges: computed.filterBy(
+    "sortedPledges",
+    "orderType",
+    "sustainer"
+  ),
+  sortedOneTimePledges: computed.filterBy(
+    "sortedPledges",
+    "orderType",
+    "onetime"
+  ),
+  mostRecentPledge: computed.reads("sortedPledges.firstObject"),
+  activePledges: computed.filterBy("sortedPledges", "isActiveMember", true),
   activeSustainingPledges: computed.filterBy(
-    'activePledges',
-    'orderType',
-    'sustainer'
+    "activePledges",
+    "orderType",
+    "sustainer"
   ),
   activeOneTimePledges: computed.filterBy(
-    'activePledges',
-    'orderType',
-    'onetime'
+    "activePledges",
+    "orderType",
+    "onetime"
   ),
-  isLapsedOneTimeMember: computed('pledges', 'activePledges', function() {
+  isLapsedOneTimeMember: computed("sortedPledges", "activePledges", function() {
     return (
-      this.get('sortedPledges.length') > 0 &&
-      this.get('activePledges').length === 0
+      this.get("sortedPledges.length") > 0 &&
+      this.get("activePledges.length") === 0
     );
   }),
+  isLapsedSustainer: computed(
+    "sortedSustainingPledges",
+    "activeSustainingPledges",
+    function() {
+      return (
+        this.get("sortedSustainingPledges.length") > 0 &&
+        this.get("activeSustainingPledges.length") === 0
+      );
+    }
+  ),
   showPaymentHistory: false,
   mostRecentSustainingPledgesPerOrderCode: computed(
-    'activeSustainingPledges',
+    "activeSustainingPledges",
     function() {
       let orderCodes = [];
       let latestPledgePerOrderCode = [];
-      this.get('activeSustainingPledges').forEach(pledge => {
-        let pledgeOrderCode = Ember.get(pledge, 'orderCode');
+      this.get("activeSustainingPledges").forEach(pledge => {
+        let pledgeOrderCode = Ember.get(pledge, "orderCode");
         if (!orderCodes.includes(pledgeOrderCode)) {
           orderCodes.push(pledgeOrderCode);
           latestPledgePerOrderCode.push(pledge);
@@ -45,6 +62,6 @@ export default Ember.Component.extend({
     }
   ),
   hasMadeRecentPledge: computed(function() {
-    return getCookie('recentPledge') === 'true';
-  }),
+    return getCookie("recentPledge") === "true";
+  })
 });
