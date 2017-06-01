94de633fccdbeb4dc55a913fdf08efcd442e1988
Brian Whitton <me@brianwhitton.com>
2017-06-01 18:21:39 -0400

    moved nypr-surey-banner into nypr-ui as nypr-floating-banner

--- addon/components/nypr-survey-banner.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-import layout from '../templates/components/nypr-survey-banner';
-
-export default Ember.Component.extend({
-  layout,
-  tagName: ''
-});
--- app/components/nypr-survey-banner.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'nypr-account-settings/components/nypr-survey-banner';
