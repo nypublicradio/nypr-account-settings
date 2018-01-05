d1195a404da5e3b767e86e2be774a18cf7ce7a14
Michael Hearn <git@mikehearn.net>
2018-01-04 21:58:12 -0500

    Tax letter link with tests

--- addon/components/nypr-accounts/membership-card.js
+++ addon/components/nypr-accounts/membership-card.js
@@ -1,6 +1,23 @@
 import Ember from "ember";
 import layout from "../../templates/components/nypr-accounts/membership-card";
+import computed from "ember-computed";
+import moment from "moment";
 
 export default Ember.Component.extend({
   layout,
+  taxLetterUrl: computed(function() {
+    let config = Ember.getOwner(this).resolveRegistration('config:environment');
+    let urlRoot = config.membershipAPI || "https://api.wnyc.org";
+    return `${urlRoot}/v1/taxes`;
+  }),
+  previousYear: computed(function() {
+    return moment()
+      .subtract(1, "year")
+      .year();
+  }),
+  hasPledgesInPastTaxYear: computed.filter("pledges", function(pledge) {
+    let previousYear = moment().subtract(1, "year").year();
+    let pledgeYear = pledge.orderDate || pledge.get("orderDate").year();
+    return moment(pledgeYear == previousYear);
+  })
 });
---
+++ tests/acceptance/download-tax-letter-test.js
@@ -0,0 +1,59 @@
+import { test } from "qunit";
+import moduleForAcceptance from "../../tests/helpers/module-for-acceptance";
+import moment from "moment";
+
+moduleForAcceptance("Acceptance | download tax letter");
+
+test("tax letter link is visible", function(assert) {
+  let lastYear = moment().subtract(1, "year").year();
+  server.create("user");
+  server.createList("pledge", 20);
+  visit("/");
+
+  andThen(function() {
+    assert.equal(
+      find(".tax-letter-link")
+        .text()
+        .trim(),
+      `${lastYear} Tax Receipt`
+    );
+  });
+});
+
+test("tax letter links to the expected url endpoint", function(assert) {
+  server.create("user");
+  server.createList("pledge", 20);
+  visit("/");
+
+  andThen(function() {
+    assert.equal(
+      find(".tax-letter-link").attr("href"),
+      "http://api.example.org/v1/taxes"
+    );
+  });
+});
+
+test("tax letter is not visible if no pledges", function(assert) {
+  server.create("user");
+  server.createList('pledge', 0);
+  visit("/");
+
+  andThen(function() {
+    assert.ok(find(".tax-letter-link").empty());
+  });
+
+});
+
+test("tax letter is not visible if pledges from ineligible year", function(
+  assert
+) {
+  server.create("user");
+  server.createList("pledge", 20, {
+    orderDate: "2015-10-28T07:34:00.502Z"
+  });
+  visit("/");
+
+  andThen(function() {
+    assert.ok(find(".tax-letter-link").empty());
+  });
+});
