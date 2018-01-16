ddea4df22fa76999c2f133d66ba99f1bddc75161
Michael Hearn <git@mikehearn.net>
2018-01-16 15:05:03 -0500

    Back to the XMLHttpRequest version

--- addon/components/nypr-accounts/membership-card/download-tax-link-modal.js
+++ addon/components/nypr-accounts/membership-card/download-tax-link-modal.js
@@ -1,6 +1,6 @@
 import Ember from "ember";
 import computed from "ember-computed";
-import fetch from 'fetch';
+import fetch from "fetch";
 import layout from "../../../templates/components/nypr-accounts/membership-card/download-tax-link-modal";
 import service from "ember-service/inject";
 
@@ -15,27 +15,67 @@ export default Ember.Component.extend({
   init() {
     this.get("session").authorize("authorizer:nypr", (header, value) => {
       let taxLetterUrl = this.get("taxLetterUrl");
-      let requestHeaders = {};
-      requestHeaders[header] = value;
+      let that = this;
 
-      fetch(taxLetterUrl, {headers: requestHeaders}).then(response => {
-        response.arrayBuffer().then(data => {
-          var blob = new Blob([data], {type: "application/pdf"});
-          var url = window.URL.createObjectURL(blob);
-          var a = document.createElement("a");
-          var el = document.getElementById("tax-receipt-modal-body")
+      // https://stackoverflow.com/questions/16086162/handle-file-download-from-ajax-post
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", taxLetterUrl, true);
+      xhr.setRequestHeader(header, value);
+      xhr.responseType = "arraybuffer";
+      xhr.onload = function() {
+        if (this.status === 200) {
+          var filename = "";
+          var disposition = xhr.getResponseHeader("Content-Disposition");
+          if (disposition && disposition.indexOf("attachment") !== -1) {
+            var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+            var matches = filenameRegex.exec(disposition);
+            if (matches != null && matches[1])
+              filename = matches[1].replace(/['"]/g, "");
+          }
+          var type = xhr.getResponseHeader("Content-Type");
 
-          el.appendChild(a);
-          a.classList.add('nypr-account-confirm')
-          a.href = url;
-          a.innerText = "Download Tax Document";
-          a.download = "NYPR_Tax_Documentation.pdf";
+          var blob =
+            typeof File === "function"
+              ? new File([this.response], filename, { type: type })
+              : new Blob([this.response], { type: type });
+          if (typeof window.navigator.msSaveBlob !== "undefined") {
+            // IE workaround for "HTML7007: One or more blob URLs were
+            // revoked by closing the blob for which they were created. These
+            // URLs will no longer resolve as the data backing the URL has
+            // been freed."
+            window.navigator.msSaveBlob(blob, filename);
+          } else {
+            var URL = window.URL || window.webkitURL;
+            var downloadUrl = URL.createObjectURL(blob);
 
-          this.set("taxReceiptReady", true);
-        });
-      })
+            if (filename) {
+              // use HTML5 a[download] attribute to specify filename
+              var a = document.createElement("a");
+              // safari doesn't support this yet
+              if (typeof a.download === "undefined") {
+                window.location = downloadUrl;
+              } else {
+                a.href = downloadUrl;
+                a.download = filename;
+              }
+            } else {
+              window.location = downloadUrl;
+            }
 
+            // Add a href to modal
+            var el = document.getElementById("tax-receipt-modal-body");
+            el.appendChild(a);
+            a.classList.add("nypr-account-confirm");
+            a.innerText = "Download Tax Document";
+
+            that.set("taxReceiptReady", true);
+          }
+        }
+      };
+      xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xhr.send();
     });
+
     this._super();
   }
 });
