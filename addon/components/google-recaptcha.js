8d1ae5728ec19f397c7cb1bccc9962efc84cae4e
Michael Hearn <git@mikehearn.net>
2018-07-31 10:28:30 -0400

    Minor code cleanup

--- addon/components/google-recaptcha.js
+++ addon/components/google-recaptcha.js
@@ -2,25 +2,24 @@ import Component from "@ember/component";
 import layout from "../templates/components/google-recaptcha";
 import { alias } from "@ember/object/computed";
 import { isNone } from "@ember/utils";
-import { later } from "@ember/runloop";
+import { bind, later } from "@ember/runloop";
 import { merge } from "@ember/polyfills";
 import { isPresent } from "@ember/utils";
 import { next } from "@ember/runloop";
 
 export default Component.extend({
+  /**
+   * Component based off ember-g-recaptcha ember component:
+   * https://github.com/algonauti/ember-g-recaptcha
+  */
   layout,
-
   classNames: ["g-recaptcha"],
-
-  sitekey: "6LeJomQUAAAAABVGp6Xk3PUZGXNWaHo3t1D7mwF3",
-
+  sitekey: "6LeJomQUAAAAABVGp6Xk3PUZGXNWaHo3t1D7mwF3", // public captcha key
   tabindex: alias("tabIndex"),
 
   renderReCaptcha() {
     if (isNone(window.grecaptcha) || isNone(window.grecaptcha.render)) {
-      later(() => {
-        this.renderReCaptcha();
-      }, 500);
+      later(this, 'renderRecaptcha', 500);
     } else {
       let container = this.element;
       if (container) {
@@ -33,20 +32,17 @@ export default Component.extend({
           "hl"
         );
         let parameters = merge(properties, {
-          callback: this.get("successCallback").bind(this),
-          "expired-callback": this.get("expiredCallback").bind(this)
+          callback: bind(this, "successCallback"),
+          "expired-callback": bind(this, "expiredCallback")
         });
         let widgetId = window.grecaptcha.render(container, parameters);
         this.set("widgetId", widgetId);
-        this.set("ref", this);
       }
     }
   },
 
   resetReCaptcha() {
-    if (isPresent(this.get("widgetId"))) {
-      window.grecaptcha.reset(this.get("widgetId"));
-    }
+    window.grecaptcha.reset(this.get("widgetId"));
   },
 
   successCallback(recaptchaKey) {
@@ -54,19 +50,18 @@ export default Component.extend({
   },
 
   expiredCallback() {
-    let action = this.get("onExpired");
-    if (isPresent(action)) {
-      action();
-    } else {
-      this.resetReCaptcha();
-    }
+    this.resetReCaptcha();
   },
-
-  // Lifecycle Hooks
 
   didInsertElement() {
     function getScript(source, callback) {
-      // getScript snippet taken from https://stackoverflow.com/a/28002292
+      /**
+       * Loads and executes a script asynchronously; using this here so the
+       * reCaptcha script isn't executed on every page, but only when the user
+       * hits the sign up page.
+       *
+       * getScript snippet taken from https://stackoverflow.com/a/28002292
+      */
       var script = document.createElement("script");
       var prior = document.getElementsByTagName("script")[0];
       script.async = 1;
@@ -80,8 +75,8 @@ export default Component.extend({
           script.onload = script.onreadystatechange = null;
           script = undefined;
 
-          if (!isAbort) {
-            if (callback) callback();
+          if (!isAbort && callback) {
+            callback();
           }
         }
       };
